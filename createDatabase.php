0783aca064e3f83a658443150e3cc3f7eb021055
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-01 12:45:59 -0400

    Finished setup script for mySql

--- createDatabase.php
+++ createDatabase.php
@@ -10,7 +10,8 @@
 	//Create Tables if they don't exist
 	$dbh->query("CREATE TABLE IF NOT EXISTS `types` (pkId INT(10) AUTO_INCREMENT PRIMARY KEY, description VARCHAR(20) );");
 	$dbh->query("CREATE TABLE IF NOT EXISTS `pins` ( pkId INT(10) AUTO_INCREMENT PRIMARY KEY, lon VARCHAR(64), lat VARCHAR(64), fkType INT(10) REFERENCES types(pkId));");
- 
+	$dbh->query("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
+	$dbh->query("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
 
 
 
