583fda2d0b42169763fa8e562798f2c0222e3671
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-01 13:05:03 -0400

    Database creation script tested and working. Remember to create a config script

--- createDatabase.php
+++ createDatabase.php
@@ -4,14 +4,14 @@
 	$dbh = new PDO('mysql:host='.HOST, DB_USER, DB_PASS);
 
 	//Create the database if it doesn't exist
-	$dbh->query("CREATE DATABASE IF NOT EXISTS " . DB_NAME);
-	$dbh->query("USE " . DB_NAME);
+	echo $dbh->exec("CREATE DATABASE IF NOT EXISTS " . DB_NAME);
+	echo $dbh->exec("USE " . DB_NAME);
 
 	//Create Tables if they don't exist
-	echo $dbh->query("CREATE TABLE IF NOT EXISTS `types` (pkId INT(10) AUTO_INCREMENT PRIMARY KEY, description VARCHAR(20) );");
-	echo $dbh->query("CREATE TABLE IF NOT EXISTS `pins` ( pkId INT(10) AUTO_INCREMENT PRIMARY KEY, lon VARCHAR(64), lat VARCHAR(64), fkType INT(10) REFERENCES types(pkId));");
-	echo $dbh->query("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
-	echo $dbh->query("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
+	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `types` (pkId INT(10) AUTO_INCREMENT PRIMARY KEY, description VARCHAR(20) );");
+	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `pins` ( pkId INT(10) AUTO_INCREMENT PRIMARY KEY, lon VARCHAR(64), lat VARCHAR(64), fkType INT(10) REFERENCES types(pkId));");
+	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
+	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
 
 
 
