4eb9f57617f284dabc843a148e032d5887c27bb7
Scott MacEwan <wsmacewan101@gmail.com>
2013-06-01 15:15:38 -0400

    added procedure to createDatabase, basic grid class

--- createDatabase.php
+++ createDatabase.php
@@ -13,7 +13,4 @@
 	echo $dbh->query("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
 	echo $dbh->query("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
 	echo $dbh->query("CREATE PROCEDURE incrementTime(lat_pk FLOAT(10),long_pk FLOAT(10),time_spent_new FLOAT(6)) BEGIN INSERT INTO grid (pkLat, pkLong, secondsWorked) VALUES (lat_pk, long_pk, time_spent_new) ON DUPLICATE KEY UPDATE secondsWorked = secondsWorked+time_spent_new; END;");
-
-
-
 ?>
