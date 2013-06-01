8aed4a23d7207d517421f2ea212f207151e5f374
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-01 13:53:45 -0400

    Updated database grid table to have lon lattitude as keys for simple grid system

--- server/createDatabase.php
+++ server/createDatabase.php
@@ -10,7 +10,7 @@
 	//Create Tables if they don't exist
 	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `types` (pkId INT(10) AUTO_INCREMENT PRIMARY KEY, description VARCHAR(20) );");
 	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `pins` ( pkId INT(10) AUTO_INCREMENT PRIMARY KEY, lon VARCHAR(64), lat VARCHAR(64), fkType INT(10) REFERENCES types(pkId));");
-	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
+	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `grid` ( pkLat FLOAT(5,10), pkLon FLOAT(5,10), secondsWorked FLOAT(6), PRIMARY KEY(pkLat,pkLon) );") ;
 	echo $dbh->exec("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
 
 
