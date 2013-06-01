ec8d3dc03626090e80d993cc45146adcb9000acf
Scott MacEwan <wsmacewan101@gmail.com>
2013-06-01 14:34:36 -0400

    created procedure for updating timespent in location

--- createDatabase.php
+++ createDatabase.php
@@ -12,6 +12,7 @@
 	echo $dbh->query("CREATE TABLE IF NOT EXISTS `pins` ( pkId INT(10) AUTO_INCREMENT PRIMARY KEY, lon VARCHAR(64), lat VARCHAR(64), fkType INT(10) REFERENCES types(pkId));");
 	echo $dbh->query("CREATE TABLE IF NOT EXISTS `grid` ( pkZone CHAR(8) PRIMARY KEY, secondsWorked FLOAT(6) );") ;
 	echo $dbh->query("CREATE TABLE IF NOT EXISTS `talk` ( pkId INT(16) AUTO_INCREMENT PRIMARY KEY, message TEXT );");
+	echo $dbh->query("CREATE PROCEDURE incrementTime(lat_pk FLOAT(10),long_pk FLOAT(10),time_spent_new FLOAT(6)) BEGIN INSERT INTO grid (pkLat, pkLong, secondsWorked) VALUES (lat_pk, long_pk, time_spent_new) ON DUPLICATE KEY UPDATE secondsWorked = secondsWorked+time_spent_new; END;");
 
 
 
