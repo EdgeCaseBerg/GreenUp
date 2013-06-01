216a19f4800f0a8f61196e25d4cf37d2f0b50131
Scott MacEwan <wsmacewan101@gmail.com>
2013-06-01 16:15:52 -0400

    Made it so the seconds spent in a location are calculated server side

--- server/addGridData.php
+++ server/addGridData.php
@@ -3,9 +3,17 @@ include('Grid.php');
 
 $data = json_decode($_POST['data']);
 $grid = new Grid();
-foreach($data as $point)
-{
-	$grid->incrementTime($point['lat'], $point['long'], $point['secondsSpent']);
+$previousPoint = $data[0];
+for($i=1; $i<sizeof($data); $i++)
+{	
+	if(round($data[$i]['lat'],4) !== round($previousPoint['lat'],4) || round($data[$i]['long'],4) !== round($previousPoint['long'],4) || $i = (sizeof($data)-1))
+	{
+		$grid->incrementTime($previousPoint['lat'], $previousPoint['long'], ($data[$i]['seconds']- $previousPoint['seconds']));
+		$previousPoint = $data[$i];
+	}
+	
 }
+
+echo json_encode(true);
 
 ?>
