7a13a0680e840bfc3fe76b37410cfd26124be042
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-08 15:36:24 -0400

    Made the map panTo nearer the user's location when they click 'start cleaning up'

--- client/js/mapsUI.js
+++ client/js/mapsUI.js
@@ -24,8 +24,11 @@ var MOUSEUP_TIME;
                 {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
                 {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
             ];
+
+
             
 function initialize() {
+
     var centerPoint = new google.maps.LatLng(37.774546, -122.433523); 
     var mapOptions = {
     zoom: 13,
