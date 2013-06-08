bddd4441ac03dd2e9844a088d7a25c0618dc09f0
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-08 19:01:21 -0400

    added markers loading from server, as well as their messages (if they have one) being loaded to an info window when clicked #2

--- server/getPointsOfInterest.php
+++ server/getPointsOfInterest.php
@@ -4,7 +4,7 @@
 	//can't do yet becuase we haven't gotten the edge locations of the map yet.
 	require_once('config.php');
 	$dbh = new PDO('mysql:host='.HOST.';dbname='.DB_NAME.';', DB_USER, DB_PASS);
-	$getPins = "SELECT lat,lon,fkType from pins" ;
+	$getPins = "SELECT lat,lon,pins.fkType as fkType,message from pins,talk where talk.pkId = pins.fkTalkId" ;
 	$statment = $dbh->query($getPins);
 	$returned = $statment->fetchAll(PDO::FETCH_ASSOC);
 
