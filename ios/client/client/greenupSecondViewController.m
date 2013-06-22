e173bc896347296fcf0f6610b12e9f72541ac231
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2013-06-22 09:22:56 -0400

    added baseline ios project file

---
+++ ios/client/client/greenupSecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  greenupSecondViewController.m
+//  client
+//
+//  Created by Joshua Dickerson on 6/22/13.
+//  Copyright (c) 2013 GreenUp. All rights reserved.
+//
+
+#import "greenupSecondViewController.h"
+
+@interface greenupSecondViewController ()
+
+@end
+
+@implementation greenupSecondViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ios/client/clientTests/clientTests.h
@@ -0,0 +1,13 @@
+//
+//  clientTests.h
+//  clientTests
+//
+//  Created by Joshua Dickerson on 6/22/13.
+//  Copyright (c) 2013 GreenUp. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface clientTests : SenTestCase
+
+@end
