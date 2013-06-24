bfdcd280245632c5ca5a87b426090b19e363a820
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-24 18:34:45 -0400

    Began adding semantic and syntactic request checking to comments endpoint

--- api/comments.py
+++ api/comments.py
@@ -1,22 +1,36 @@
-#Import google sign in
-from google.appengine.ext import db
-
 import webapp2
 import json
 
 import api
 
 
+COMMENT_TYPES = ['FORUM', 'NEEDS', 'MESSAGE']
 
 class Comments(webapp2.RequestHandler):
 
 	def get(self):
-		#TODO
+		#Default status if none are set
 		self.response.set_status(api.HTTP_NOT_IMPLEMENTED,"")
+
+		#Check for optional parameters:
+		commentType = self.request.get("type")
+		if commentType:
+			#We have an optional parameter. Is it well formed?
+			if commentType.upper() in COMMENT_TYPES:
+				#Yes it is well formed and we may execute a datastore query for the comments
+				self.response.set_status(api.HTTP_OK,"")
+			else:
+				#Semantically incorrect query
+				self.response.set_status(api.HTTP_REQUEST_SEMANTICS_PROBLEM,"")
+
+
+		#TODO
+		
 		self.response.write("{}")	
 
 	def post(self):
-		#TODO
+		#Confirm that all elements of the json are there.
+		
 		self.response.set_status(api.HTTP_NOT_IMPLEMENTED,"")
 		self.response.write("{}")
 
