f511de187e69205f8f35609d0bafb8f5e1bda57e
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-06-24 20:56:33 -0400

    Added code for dealing with posting a comment without actually creating one.

--- api/comments.py
+++ api/comments.py
@@ -2,6 +2,7 @@ import webapp2
 import json
 
 import api
+import logging
 
 
 COMMENT_TYPES = ['FORUM', 'NEEDS', 'MESSAGE']
@@ -18,7 +19,9 @@ class Comments(webapp2.RequestHandler):
 			#We have an optional parameter. Is it well formed?
 			if commentType.upper() in COMMENT_TYPES:
 				#Yes it is well formed and we may execute a datastore query for the comments
-				self.response.set_status(api.HTTP_OK,"")
+				pass
+				#TODO: Eventually return this response once things are done
+				#self.response.set_status(api.HTTP_OK,"")
 			else:
 				#Semantically incorrect query
 				self.response.set_status(api.HTTP_REQUEST_SEMANTICS_PROBLEM,"")
@@ -29,9 +32,44 @@ class Comments(webapp2.RequestHandler):
 		self.response.write("{}")	
 
 	def post(self):
-		#Confirm that all elements of the json are there.
-		
 		self.response.set_status(api.HTTP_NOT_IMPLEMENTED,"")
+
+		#Confirm that all elements of the json are there.
+		logging.info(self.request.body)
+		try:
+			json.loads(self.request.body)
+		except Exception, e:
+			#The request body is malformed. 
+			self.response.set_status(api.HTTP_REQUEST_SYNTAX_PROBLEM,"")
+			self.response.write("{}")
+			#Don't allow execution to proceed any further than this
+			return
+		info = json.loads(self.request.body)
+
+		#Request is well formed, but does it hold the proper semantic meaning for us? (all keys present)
+		try:
+			info['type']
+			info['message']
+		except Exception, e:
+			#The request body lacks proper keys
+			self.response.set_status(api.HTTP_REQUEST_SEMANTICS_PROBLEM,"")
+			self.response.write("{}")
+			return
+
+		#Request has proper required keys
+		typeOfComment = info['type']
+		commentMessage = info['message']
+
+		pin = None
+		try:
+			info['pin']
+			pin = info['pin']
+		except Exception, e:
+			#Die silently if the pine is not there as it is optional
+			pass
+		
+		#All information present. Store information in the database
+
 		self.response.write("{}")
 
 		
