bdea7a50d57844146be61daaecf56b48503f9121
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-07-09 07:59:17 -0400

    Added usage to script. Managed to kill my api server by running it.

--- api/cannon.sh
+++ api/cannon.sh
@@ -1,10 +1,14 @@
+#!/bin/sh
 
 echo "Cannon.sh creates 1000 curl processes against the passed in url\n"
 
 if [ "$#" -ne 1 ]
 then
  	echo "You must supply a url to send GET requests to."
-	 exit 1
+	echo "Usage: "
+	echo "./cannon.sh localhost:30002/api"
+	echo "./cannon.sh localhost:30002/api/comments"
+	exit 1
 fi
 
 for i in `seq 1 1000` ; do
