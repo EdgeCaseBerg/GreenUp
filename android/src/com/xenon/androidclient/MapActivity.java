42c9823ee1c71787cc3ac666a6fe6e4928bef39c
primehunter326 <primehunter326@gmail.com>
2013-07-13 12:19:29 -0400

    Changed android target version to 4.2.2 (API level 17) and added stubs for Map and Comment pages

---
+++ android/src/com/xenon/androidclient/MapActivity.java
@@ -0,0 +1,16 @@
+package com.xenon.androidclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
