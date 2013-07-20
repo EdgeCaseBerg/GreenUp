e840d027becf1f4ecaf707a1d050a080b414c812
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-07-20 17:46:05 -0400

    updated symlinks

--- detector.py
+++ detector.py
@@ -10,10 +10,10 @@ class Detector(webapp2.RequestHandler):
 		if ("Mobile" in uastring and "Safari" in uastring) or self.request.get('agent'):
 			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'])
 			#Render regular index
-			template = JINJA_ENVIRONMENT.get_template('index.html')
+			template = JINJA_ENVIRONMENT.get_template('mobile.html')
 		else:
 			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'])			
-			template = JINJA_ENVIRONMENT.get_template('mobile.html')
+			template = JINJA_ENVIRONMENT.get_template('index.html')
 
 		self.response.out.write(template.render())
 		
