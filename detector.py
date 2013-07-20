9507ac34c5386bc7ad000b317260b41c4d4c4871
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-07-20 15:58:00 -0400

    Added routing and symlinks. Broke the css and loading of files, but it routes on user agent properly

--- detector.py
+++ detector.py
@@ -8,14 +8,14 @@ class Detector(webapp2.RequestHandler):
 	def get(self):
 		uastring = self.request.headers.get('user_agent')
 		if "Mobile" in uastring and "Safari" in uastring:
-			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+'/web/client/'),extensions=['jinja2.ext.autoescape'])
+			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'])
 			#Render regular index
 			template = JINJA_ENVIRONMENT.get_template('index.html')
 		else:
-			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+'/web/client/PhoneFrame/'),extensions=['jinja2.ext.autoescape'])			
-			template = JINJA_ENVIRONMENT.get_template('PhoneFrame.html')
+			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'])			
+			template = JINJA_ENVIRONMENT.get_template('mobile.html')
 
-		self.response.out.write(template.render(None))
+		self.response.out.write(template.render())
 		
 application = webapp2.WSGIApplication([
 										('/', Detector), 
