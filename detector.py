9922e83df66229ffb6c2ff78aa1083289ab7768e
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-07-20 17:25:02 -0400

    removed inception frame affect. Need to redo the symlink for phoneframe... it doesn't seem like a proper one

--- detector.py
+++ detector.py
@@ -7,7 +7,7 @@ class Detector(webapp2.RequestHandler):
 
 	def get(self):
 		uastring = self.request.headers.get('user_agent')
-		if "Mobile" in uastring and "Safari" in uastring:
+		if ("Mobile" in uastring and "Safari" in uastring) or self.request.get('agent'):
 			JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'])
 			#Render regular index
 			template = JINJA_ENVIRONMENT.get_template('index.html')
