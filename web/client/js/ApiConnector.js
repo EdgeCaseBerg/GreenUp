57d284c4820dc7e92c6e80070e66488ce50cd44b
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2013-07-21 19:48:22 -0400

    switching url

--- web/client/js/ApiConnector.js
+++ web/client/js/ApiConnector.js
@@ -9,8 +9,8 @@ function ApiConnector(){
 	var commentData = [];
 
 
-	// var BASE = "http://greenupapp.appspot.com/api";
-	var BASE = "http://localhost:30002/api";
+	var BASE = "http://greenupapp.appspot.com/api";
+	// var BASE = "http://localhost:30002/api";
 	this.BASE = BASE;
 
 	// api URLs
