69afa6f21a74ae5ad5ead98f3843de7edf8523f8
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2013-08-20 20:57:59 -0400

    heatmap populating, but throwing error after 2 mins of logging

--- web/client/js/ApiConnector.js
+++ web/client/js/ApiConnector.js
@@ -258,6 +258,7 @@ function ApiConnector(){
 	    	success: function(data){
 		      	console.log("PUT heatmap success: "+data);
 	       		// window.database.nuke();
+	       		window.ApiConnector.pullHeatmapData();
 	    	}
 		});//Ajax	
 	}
@@ -312,7 +313,7 @@ function GpsHandle(){
 		window.updateCounter = 0;
     	db = Lawnchair({name : 'db'}, function(store) {
         	window.database = store;
-        	window.GPS.loggingInterval = setInterval(function() {window.GPS.runUpdate()},30000);//update user location every 5 seconds
+        	window.GPS.loggingInterval = setInterval(function() {window.GPS.runUpdate()},3000);//update user location every 30 seconds
         	// instead of running 2 timers, we'll just set a counter and run the pushHeatmapData() on a multiple of... 
         	// ...the runUpdate() function
         	// setInterval(function() {window.ApiConnector.pushHeatmapData(store)},3000);//upload locations to the server every 30 seconds
