31090066c95542e8529332a97d2610829048a64a
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-08-27 08:56:40 -0400

    Began work on home page
    Added start and stop svg's to generation script. Added logo to low res
    files becuase it's currently a PNG and not an SVG to generate from.
    
    Created basic layout for the home page, with logo, text and toggle
    button. No functionality yet. Once I have internet connection again I
    can take a look at the mock-ups and try to get the tab to look correct.

--- android/src/com/xenon/greenup/HomeSectionFragment.java
+++ android/src/com/xenon/greenup/HomeSectionFragment.java
@@ -11,7 +11,7 @@ public class HomeSectionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, 
         Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_section_dummy, container, false);
+        return inflater.inflate(R.layout.start_page, container, false);
     }
 
 }
