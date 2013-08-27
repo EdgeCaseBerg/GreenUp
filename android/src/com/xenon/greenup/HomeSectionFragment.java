3713ea3b6ca35647d011831fbfe5ecd43c4ad2f6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-08-27 18:16:41 -0400

    Updated graphics of start button
    The start button now alternates between red and green as well as
    changing text when clicked. Also, the id's in the start_page xml have
    been updated to be a bit more descriptive

--- android/src/com/xenon/greenup/HomeSectionFragment.java
+++ android/src/com/xenon/greenup/HomeSectionFragment.java
@@ -2,16 +2,39 @@ package com.xenon.greenup;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ToggleButton;
 
 public class HomeSectionFragment extends Fragment {
+	private boolean toggleState = false;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
-        Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.start_page, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+    	View rootview = inflater.inflate(R.layout.start_page, container, false);
+    	final  ToggleButton startStopButton = (ToggleButton)rootview.findViewById(R.id.start_stop_button );
+
+    	startStopButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(!toggleState) {  
+					startStopButton.setBackgroundResource(R.drawable.stop);
+					//Fire off some type of request to start the background gps polling service
+					//on a completely seperate thread at the very least.
+				} else {  
+					startStopButton.setBackgroundResource(R.drawable.start);
+				}
+				toggleState = !toggleState;
+				
+			}
+		});
+
+        return rootview;
     }
 
 }
