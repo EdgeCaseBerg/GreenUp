d4f58ca48eab724bf32463d35dca8412e7383500
Joshua.Dickerson <Joshua.Dickerson@dealer.com>
2013-09-24 14:07:50 -0400

    charts generated

--- web/dash/DashApiConnector.js
+++ web/dash/DashApiConnector.js
@@ -313,7 +313,12 @@ function MapHandle(){
 		// get our bounds and set our map as loaded
 		google.maps.event.addListener(window.MAP.map, 'idle', function(ev){
 		  	window.UI.setMapLoaded
-		  	window.MAP.updateBounds();	
+		  	window.MAP.updateBounds();
+
+		  	// google.load("visualization", "1", {packages:["corechart"]});
+      		// google.setOnLoadCallback(function(){
+      			window.UI.drawVisualisation();
+      		// });	
 			
 		});
 
@@ -648,7 +653,41 @@ function UiHandle(){
     	$('#infoIcon').click(function(){
     		window.UI.toggleMapOptions();
     	});
+
+    	$('#infoIcon').mouseenter(function(){
+    		$(this).attr("src", "images/info-icon-light.png");	
+    	});
+
+    	$('#infoIcon').mouseleave(function(){
+    		$(this).attr("src", "images/info-icon-dark.png");	
+    	});
+
+
+  
+
 	} // end init
+
+	UiHandle.prototype.drawVisualisation = function drawVisualization(){
+
+		// google.load("visualization", "1", {packages:["corechart"]});
+      	// google.setOnLoadCallback(function(){
+      			// window.UI.drawVisualisation();	
+		console.log("gggg");
+	  // Create and populate the data table.
+	  	var data =  new google.visualization.arrayToDataTable([
+	    	['Task', 'Hours per Day'],
+	    	['Work', 11],
+	    	['Eat', 2],
+	    	['Commute', 2],
+	    	['Watch TV', 2],
+	    	['Sleep', 7]
+	  	]);
+	  	  new google.visualization.PieChart(document.getElementById('usageDataChart')).draw(data, {title:"So, how was your day?"});
+
+	  // });
+
+	  // Create and draw the visualization.
+	}
 
 	UiHandle.prototype.toggleMapOptions = function toggleMapOptions(){
 		if(window.UI.isOptionsVisible){
