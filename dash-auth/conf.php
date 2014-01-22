9fe91bac098f0286d648bbc7d9c24d1040d167ea
joshuadickerson <joshuajdickerson@gmail.com>
2014-01-21 21:00:43 -0500

    ushing local conf

--- dash-auth/conf.php
+++ dash-auth/conf.php
@@ -11,5 +11,5 @@ define("DB_NAME", $config->DB_NAME);
 //define("DB_HOST", "dev.xenonapps.com");
 //define("DB_PASS", "1k23k4k55j");
 //define("DB_NAME", "green_up_vt");
-//define("BASE_INCLUDE", dirname(__FILE__) );
+define("BASE_INCLUDE", dirname(__FILE__) );
 ?>
