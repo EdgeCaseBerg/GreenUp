01df12ececa9d8f58231889af7fff1dbf30a6831
joshuadickerson <joshuajdickerson@gmail.com>
2014-01-26 21:13:30 -0500

    removed old login form

--- dash/index.php
+++ dash/index.php
@@ -52,16 +52,6 @@ if(isset($_GET['logout'])){
 
 
 
-
-
-        #loginContainer{
-           display: none;
-           height: 90%;
-           margin-top: -43px;
-           background: url('images/backgrounds/80_percent_black.png');
-           padding: 10px;
-        }
-
         #loginNest{
             font-size: 1.5em;
             width: 350px;
@@ -104,7 +94,7 @@ if(isset($_GET['logout'])){
 
         .col-md-6{
             /*border:solid 2px #fff;*/
-          }
+        }
     </style>
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -114,127 +104,83 @@ if(isset($_GET['logout'])){
         $(document).ready(function(){
 
 
-        <?
-        if($_SERVER['HTTPS'] != null){
-            echo("var base = 'https://" . $_SERVER['SERVER_NAME'] . $base . "';");
-        }else{
-            echo("var base = 'http://" . $_SERVER['SERVER_NAME'] . $base . "';");
-        }
+            <?
+            if($_SERVER['HTTPS'] != null){
+                echo("var base = 'https://" . $_SERVER['SERVER_NAME'] . $base . "';");
+            }else{
+                echo("var base = 'http://" . $_SERVER['SERVER_NAME'] . $base . "';");
+            }
 
-        if($errorMsg != ""){
-            echo "$('#loginContainer').show();";
-        }
+            echo "console.log(base);";
 
-        echo "console.log(base);";
-
-        ?>
+            ?>
 
 
 //            $('#loginForm').attr("action", base+"home.php");
 
-            $('#loginButton').click(function(){
-                $('#loginContainer').show();
-            });
-
             $('#loginCancelButton').click(function(){
                 $('#username').val("");
                 $('#password').val("");
-                $('#loginContainer').hide();
 
             });
         });
     </script>
 </head>
 <body>
-    <nav class="navbar navbar-default" role="navigation" id="headerNav">
+<nav class="navbar navbar-default" role="navigation" id="headerNav">
     <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"><img src="images/greenup-logo-redux.png" height="50" id="navLogo" /></a>
-        </div>
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#"><img src="images/greenup-logo-redux.png" height="50" id="navLogo" /></a>
+    </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
         <ul class="nav navbar-nav">
         </ul>
         <ul class="nav navbar-nav navbar-right">
             <li><button type="button"  id="loginButton" class="btn btn-default btn-md">Admin Login</button></li>
         </ul>
     </div><!-- /.navbar-collapse -->
-  </nav>
-    <div id="mainContainer">
-        <div class="row spacer100">
+</nav>
+<div id="mainContainer">
+    <div class="row spacer100">
 
-        </div>
-        <div class="row">
-            <div class="col-md-6 main_left">
-                <h2>
-                    Welcome to the Green-Up VT<br />Administrative Dashboard.
-                </h2>
-                <h4>
-                    Please log in to continue.
-                </h4>
-            </div>
-
-            <div class="col-md-6 main_right">
-                <div id="loginNest">
-                    <div class="errorMessage">
-                        <? echo $errorMsg; ?>
-                    </div>
-                    <form method="POST" name="login" id="loginForm" action="home.php">
-                        <table>
-                            <tr>
-                                <td class="inputLabel">email:</td><td><input type="email" name= "username" id="username" class="loginInput"/></td>
-                            </tr>
-
-                            <tr>
-                                <td class="inputLabel">password:</td><td><input type="password" name="password" id="password" class="loginInput"/></td>
-                            </tr>
-
-                            <tr>
-                                <td></td>
-                                <td>
-                                    <input type="submit" value="submit" id="submitButton" class="btn btn-default btn-md" style="margin-left: 10px;"/>
-                                    <input type="button" value="cancel" id="loginCancelButton" class="btn btn-default btn-md"/>
-                                </td>
-                            </tr>
-                        </table>
-                        <br /><a href="forgotPassword.php" style="font-size: 0.9em;">forgot password</a>
-                    </form>
-                </div>
-            </div>
-
+    </div>
+    <div class="row">
+        <div class="col-md-6 main_left">
+            <h2>
+                Welcome to the Green-Up VT<br />Administrative Dashboard.
+            </h2>
+            <h4>
+                Please log in to continue.
+            </h4>
         </div>
 
-
-
-
-
-        <!-- not shown  -->
-        <div id="loginContainer">
+        <div class="col-md-6 main_right">
             <div id="loginNest">
                 <div class="errorMessage">
                     <? echo $errorMsg; ?>
                 </div>
                 <form method="POST" name="login" id="loginForm" action="home.php">
-
                     <table>
                         <tr>
-                            <td class="inputLabel">email:</td><td><input type="email" name= "username" id="username"/></td>
+                            <td class="inputLabel">email:</td><td><input type="email" name= "username" id="username" class="loginInput"/></td>
                         </tr>
 
                         <tr>
-                            <td class="inputLabel">password:</td><td><input type="password" name="password" id="password"/></td>
+                            <td class="inputLabel">password:</td><td><input type="password" name="password" id="password" class="loginInput"/></td>
                         </tr>
 
                         <tr>
                             <td></td>
-                            <td><input type="submit" value="submit" id="submitButton" class="btn btn-default btn-md"/>
+                            <td>
+                                <input type="submit" value="submit" id="submitButton" class="btn btn-default btn-md" style="margin-left: 10px;"/>
                                 <input type="button" value="cancel" id="loginCancelButton" class="btn btn-default btn-md"/>
                             </td>
                         </tr>
@@ -244,4 +190,5 @@ if(isset($_GET['logout'])){
             </div>
         </div>
     </div>
+</div>
 </body>
