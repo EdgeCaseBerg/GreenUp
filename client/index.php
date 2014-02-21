d0f8dd237d304708ecc8a659c09502a04613c336
joshuadickerson <joshuajdickerson@gmail.com>
2014-02-20 19:07:32 -0500

    going to fix the URL issue

--- client/index.php
+++ client/index.php
@@ -1,9 +1,16 @@
 <?php
+// use our local config to get the path so that we can both work
+// and each use our own settings
+$confText = json_decode(file_get_contents("/etc/conf/local.conf"), 1);
+echo "<script>window.PROXY = '".$confText['proxy-path']."';console.log(window.PROXY);</script>";
+
 if(stripos($_SERVER['HTTP_USER_AGENT'], "phone") === false && stripos($_SERVER['HTTP_USER_AGENT'], "android") === false ){
     if(!isset($_GET['frame'])){
 //            header("Location: welcome.html");
     }
 }
+
+
 ?>
 
 <!DOCTYPE html>
