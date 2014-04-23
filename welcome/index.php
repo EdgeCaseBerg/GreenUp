94e40ff8eb900031b9fe2169f45f68f24088cb4c
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-22 20:22:13 -0400

    added last modified

--- welcome/index.php
+++ welcome/index.php
@@ -1,3 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo "{\"message\" : \"Welcome to Green-Up VT\", \"date\" : \"May 3, 2014\"}";
+echo "{\"message\" : \"Welcome to Green-Up VT\", \"date\" : \"May 3, 2014\", \"last_updated\" : \"1398212459\"}";
