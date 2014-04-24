23fb3649626dd2a09a01c9b35d054c888aebf312
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-23 21:15:22 -0400

    https for donate

--- dash/index.php
+++ dash/index.php
@@ -160,7 +160,7 @@ if(!isset($_COOKIE[session_name()])){
                     echo "<li><button type=\"button\"  id=\"viewLogButton\" class=\"navButtons btn btn-default btn-md\">Server Log</button></li>";
                     echo "<li><button type=\"button\"  id=\"logoutButton\" onclick=\"location.href = 'http://greenup.xenonapps.com/dash/login.php?errorno=2';\" class=\"navButtons btn btn-default btn-md\">Logout</button></li>";
                 }else{
-                    echo "<li><button type=\"button\"  onclick=\"location.href = 'http://donatenow.networkforgood.org/greenupvermont';\" id=\"donateButton\" class=\"navButtons btn btn-default btn-md\">Donate</button></li>";
+                    echo "<li><button type=\"button\"  onclick=\"location.href = 'https://donatenow.networkforgood.org/greenupvermont';\" id=\"donateButton\" class=\"navButtons btn btn-default btn-md\">Donate</button></li>";
                     echo "<li><button type=\"button\"  id=\"loginButton\" class=\"navButtons btn btn-default btn-md\">Login</button></li>";
                 }
             ?>
