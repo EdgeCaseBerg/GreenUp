7ff1acf58b0492e3a32adceceafc24fe15a74340
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-23 21:10:38 -0400

    added logout

--- dash/index.php
+++ dash/index.php
@@ -161,7 +161,7 @@ if(!isset($_COOKIE[session_name()])){
                     echo "<li><button type=\"button\"  id=\"logoutButton\" onclick=\"location.href = 'login.php?errorno=2';\" class=\"navButtons btn btn-default btn-md\">Logout</button></li>";
                 }else{
                     echo "<li><button type=\"button\"  onclick=\"location.href = 'donatenow.networkforgood.org/greenupvermont';\" id=\"donateButton\" class=\"navButtons btn btn-default btn-md\">Donate</button></li>";
-                    echo "<li><button type=\"button\"  id=\"loginButton\" class=\"navButtons btn btn-default btn-md\">Login</button></li>
+                    echo "<li><button type=\"button\"  id=\"loginButton\" class=\"navButtons btn btn-default btn-md\">Login</button></li>";
                 }
             ?>
 
