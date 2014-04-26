5457117eec5b706e5701ab8a20b286ce3ae9fcb2
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-26 19:03:00 -0400

    added phoneframe back

--- client/welcome.php
+++ client/welcome.php
@@ -120,10 +120,20 @@
     <div id="descriptionNest">
         <p class="contentHeader">
             <img src="img/greenup_logo.png" id="greenup_logo"/><br />
-            &nbsp;&nbsp;&nbsp;The GreenUp-VT app available for iOS, Android, and mobile web browsers, is a not-for-profit project developed by <a href="http://xenonapps.com">XenonApps.com</a> for the Green Up Vermont organization, meant to make the annual Green-Up day more efficient and better organized, while providing valuable data to help coordinate future Green-Up days. <br /><br />&nbsp;&nbsp;&nbsp;<a href="http://www.greenupvermont.org/">Green Up Vermont</a> is a nonprofit organization with 501(c)(3) status. Green Up's mission is to promote the stewardship of our state's natural landscape and waterways and the livability of our communities by involving people in Green Up Day and raising public awareness about the benefits of a litter-free environment.
+            &nbsp;&nbsp;&nbsp;The GreenUp-VT app available for iOS, Android, and mobile web browsers,
+            is a not-for-profit project developed by <a href="http://xenonapps.com">XenonApps.com
+            </a> for the Green Up Vermont organization, meant to make the annual Green-Up day more
+            efficient and better organized, while providing valuable data to help coordinate future Green-Up days.
+            <br /><br />&nbsp;&nbsp;&nbsp;<a href="http://www.greenupvermont.org/">Green Up Vermont</a>
+            is a nonprofit organization with 501(c)(3) status. Green Up's mission is to promote the stewardship of
+            our state's natural landscape and waterways and the livability of our communities by involving people in
+            Green Up Day and raising public awareness about the benefits of a litter-free environment.
             <br /><br />
-            &nbsp;&nbsp;&nbsp;The app anonymously gathers GPS data from your mobile device while you work, which, when compiled with the data of other participants, is plotted as a heat map, providing all participants with a comprehensive overview of the areas that have already been cleaned and the areas that require more attention. Furthermore, the app allows participants to comment on the needs of specific locations with geocoded messages aggregated in our comments section.
-        <h3>Click on the app to begin a tour</h3>
+            &nbsp;&nbsp;&nbsp;The app anonymously gathers GPS data from your mobile device while you work,
+            which, when compiled with the data of other participants, is plotted as a heat map, providing
+            all participants with a comprehensive overview of the areas that have already been cleaned and
+            the areas that require more attention. Furthermore, the app allows participants to comment on the
+            needs of specific locations with geo-coded messages aggregated in our comments section.
 
         </p>
     </div>
@@ -136,7 +146,7 @@
             </div>
             <!-- <iframe id="iframe" src="https://dev.xenonapps.com/green-web/client/index.php?frame=true"></iframe> -->
             <!-- <iframe id="iframe" src="http://greenupapp.appspot.com?agent=frame"></iframe>  -->
-            <iframe id="iframe" src="http://localhost/client/index.php?frame=true"></iframe>
+            <iframe id="iframe" src="http://greenup.xenonapps.com/client/index.php?frame=true"></iframe>
 
             <!-- // app content here -->
         </div>
