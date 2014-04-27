dea7980697022db965796fd313cb1c0807f3d6ac
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-27 09:55:58 -0400

    added analytics to phone frame

--- client/welcome.php
+++ client/welcome.php
@@ -101,13 +101,12 @@
             $('#iframe').attr("src", "http://greenup.xenonapps.com/client/index.php?frame=true");
         });
 
-
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-        ga('create', 'UA-44570622-1', 'greenupapp.appspot.com');
+        ga('create', 'UA-47079120-6', 'xenonapps.com');
         ga('send', 'pageview');
 
 
