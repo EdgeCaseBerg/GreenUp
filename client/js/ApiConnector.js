bf2802065824b7b0a28eb2ae06034118b4ee49a1
joshuadickerson <joshuajdickerson@gmail.com>
2014-04-26 20:13:54 -0400

    messing with admin icons

--- client/js/ApiConnector.js
+++ client/js/ApiConnector.js
@@ -631,14 +631,13 @@ function MapHandle(){
         switch(markerType){
             case "HAZARD":
                 pin.type = "HAZARD";
-                iconUrl = "img/icons/orangeCircle.png";
+                iconUrl = "img/icons/new_trash1.png";
                 break;
             case "COMMENT":
                 pin.type = "COMMENT";
                 iconUrl = "img/icons/greenCircle.png";
                 break;
             case "ADMIN":
-                alert("admin");
                 pin.type = "ADMIN";
                 iconUrl = "img/icons/blueCircle.png";
                 break;
