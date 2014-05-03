b9195bb0f44dfc6b8969c1563da3ff0edf2911bd
joshuadickerson <joshuajdickerson@gmail.com>
2014-05-03 14:21:12 -0400

    client heatmap

--- client/js/ApiConnector.js
+++ client/js/ApiConnector.js
@@ -596,20 +596,20 @@ function MapHandle(){
         console.log("Heatmap data to be applied to map: ");
         console.log(data);
         var dataObj = data;
-//        var heatmapData = [];
+        var heatmapData = [];
         // console.log(dataObj[ii].latDegrees);
 //        if(!window.HELPER.isNull(dataObj.grid)){
             for(var ii=0; ii<dataObj.grid.length; ii++){
-                window.heatmapData.push({location: new google.maps.LatLng(dataObj.grid[ii].latDegrees, dataObj.grid[ii].lonDegrees), weight: dataObj.grid[ii].secondsWorked});
+                heatmapData.push({location: new google.maps.LatLng(dataObj.grid[ii].latDegrees, dataObj.grid[ii].lonDegrees), weight: dataObj.grid[ii].secondsWorked});
 
             }
 //        }
 
         console.log("Processed heatmap data:");
-        console.log(window.heatmapData);
+        console.log(heatmapData);
 
-        if(window.heatmapData.length > 0 && window.IS_HM_LOADED){
-            var pointArray = new google.maps.MVCArray(window.heatmapData);
+        if(heatmapData.length > 0){
+            var pointArray = new google.maps.MVCArray(heatmapData);
 
             window.MAP.heatmap = new google.maps.visualization.HeatmapLayer({
                 data: pointArray,
