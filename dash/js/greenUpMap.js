ed4209e65d4ce948b844a76f95674b00987453a9
joshuadickerson <joshuajdickerson@gmail.com>
2014-05-03 16:03:48 -0400

    gradient

--- dash/js/greenUpMap.js
+++ dash/js/greenUpMap.js
@@ -129,12 +129,32 @@ function MapHandle(){
         if(window.heatmapData.length > 0 && window.IS_HM_LOADED){
             console.log("num heatmap points: "+window.heatmapData.length);
             var POINT_ARR = new google.maps.MVCArray(window.heatmapData);
+
+            var gradient = [
+                'rgba(0, 255, 255, 0)',
+                'rgba(0, 255, 255, 1)',
+                'rgba(0, 191, 255, 1)',
+                'rgba(0, 127, 255, 1)',
+                'rgba(0, 63, 255, 1)',
+                'rgba(0, 0, 255, 1)',
+                'rgba(0, 0, 223, 1)',
+                'rgba(0, 0, 191, 1)',
+                'rgba(0, 0, 159, 1)',
+                'rgba(0, 0, 127, 1)',
+                'rgba(63, 0, 91, 1)',
+                'rgba(127, 0, 63, 1)',
+                'rgba(191, 0, 31, 1)',
+                'rgba(255, 0, 0, 1)'
+            ]
+
             window.MAP.heatmap = new google.maps.visualization.HeatmapLayer({
                 data: POINT_ARR,
                 dissipating: true,
                 opacity: 0.99,
                 radius: 5
             });
+
+            window.MAP.heatmap.set('gradient', gradient);
 
             if(window.IS_HM_LOADED){
                 console.log("hm loaded - applying");
