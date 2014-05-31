aa57b88c0c902cd39771fde574e84bb1386eb282
Aaron Miller <aaron@aaronmiller.in>
2014-05-31 14:39:25 -0400

    Adding #define for HomeControler

--- ios/GoGreen2/SkinConstants.h
+++ ios/GoGreen2/SkinConstants.h
@@ -9,6 +9,13 @@
 #ifndef GreenUpVt_SkinConstants_h
 #define GreenUpVt_SkinConstants_h
 
+#define HOME_VIEW_MAIN_LABEL_TEXT @"Track your clean up progress\nSee what areas need the most help\nFind an area to drop off what you pick up"
+#define HOME_VIEW_CLEAN_UP_TOGGLE_TITLE @"Start Cleaning"
+#define HOME_VIEW_HOME_MESSAGE @"nil" //todo this is dynamic, so something more complex than this is required
+#define HOME_VIEW_NO_CXN_ALERT_TITLE @"Cannot Start Cleaning"
+#define HOME_VIEW_NO_CXN_ALERT_MESSAGE @"You dont appear to have a network connection, please connect and try and start cleaning"
+
+
 #define MESSAGE_VIEW_ALERT_CONFIRMATION_YES @"Yes, I'm Sure"
 #define MESSAGE_VIEW_ALERT_CONFIRMATION_NO @"No"
 #define MESSAGE_VIEW_ALERT_CONFIRMATION_TITLE @"Are You Sure?"
