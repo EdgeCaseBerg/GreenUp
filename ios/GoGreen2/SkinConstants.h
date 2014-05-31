e004dc0507e2f37f73cce41363d1c3d8eb4f9a1f
Aaron Miller <aaron@aaronmiller.in>
2014-05-31 13:59:06 -0400

    Moving constants to separate header file.

---
+++ ios/GoGreen2/SkinConstants.h
@@ -0,0 +1,18 @@
+//
+//  SkinConstants.h
+//  GreenUpVt
+//
+//  Created by Aaron Miller on 5/31/14.
+//  Copyright (c) 2014 Xenon Apps. All rights reserved.
+//
+
+#ifndef GreenUpVt_SkinConstants_h
+#define GreenUpVt_SkinConstants_h
+
+#define MESSAGE_VIEW_ALERT_CONFIRMATION_YES @"Yes, I'm Sure"
+#define MESSAGE_VIEW_ALERT_CONFIRMATION_NO @"No"
+#define MESSAGE_VIEW_ALERT_CONFIRMATION_TITLE @"Are You Sure?"
+#define MESSAGE_VIEW_ALERT_CONFIRMATION_MESSAGE_ADDRESSED @"Are you sure you have cleaned up this location?"
+#define MESSAGE_VIEW_ALERT_CONFIRMATION_MESSAGE_UNADDRESSED @"Are you sure this location has not been cleaned up?"
+
+#endif
